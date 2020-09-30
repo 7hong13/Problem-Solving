e0d1b9aa883f32c17d70a71fde21fe9063a78b92
김지현 <ledi000@naver.com>
2020-10-01 01:12:23 +0900

    Sorting coordinates in x-axis ascending order

---
+++ Sorting/11650/11650.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+struct Coordinate {
+    int x;
+    int y;
+};
+bool compare(Coordinate dot1, Coordinate dot2) {
+    if (dot1.x == dot2.x) return dot1.y < dot2.y;
+    return dot1.x < dot2.x;
+};
+Coordinate arrForSorting[100001];
+
+int main() {
+    int n;
+    cin >> n;
+    for (int idx = 0; idx < n; idx++) {
+        cin >> arrForSorting[idx].x >> arrForSorting[idx].y;
+    }
+    sort(arrForSorting, arrForSorting + n, compare);
+      for (int idx = 0; idx < n; idx++) {
+        cout << arrForSorting[idx].x << " " <<  arrForSorting[idx].y << "\n";
+    }
+    return 0;
+}
