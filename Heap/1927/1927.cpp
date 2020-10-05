9ef6d24a8bf579e7137166c0850a956d6c36773b
김지현 <ledi000@naver.com>
2020-10-05 17:21:28 +0900

    Min heap

---
+++ Heap/1927/1927.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+priority_queue<int, vector<int>, greater<int>> pq;
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(0);
+    int n;
+    cin >> n;
+    while (n--) {
+        int x;
+        cin >> x;
+        if (!x) {
+            int result;
+            if (pq.empty()) {
+                result = 0;
+            }
+            else {
+                result = pq.top();
+                pq.pop();
+            }
+            cout << result << "\n";
+        }
+        else {
+            pq.push(x);
+        }
+    }
+    return 0;
+}
