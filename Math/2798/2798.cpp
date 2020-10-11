34d5bd7f5fa56eb50ca7f0530e78bc68ed6b2938
김지현 <ledi000@naver.com>
2020-10-12 01:36:03 +0900

    Blackjack(Brute Force)

---
+++ Math/2798/2798.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int cards[300001];
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(0);
+    int n, m;
+    cin >> n >> m;
+    for (int idx = 0; idx < n; idx++) {
+        cin >> cards[idx];
+    }
+    int sum = -999999999;
+    for (int card1 = 0; card1 < n - 2; card1++) {
+        for (int card2 = card1 + 1; card2 < n - 1; card2++) {
+            for (int card3 = card2 + 1; card3 < n; card3++) {
+                int tempSum = cards[card1] + cards[card2] + cards[card3];
+                int diff1 = abs(sum - m);
+                int diff2 = abs(tempSum - m);
+                if (tempSum <= m && diff2 < diff1) {
+                    sum = cards[card1] + cards[card2] + cards[card3];
+                }
+            }
+        }
+    }
+    cout << sum << "\n";
+    return 0;
+}
