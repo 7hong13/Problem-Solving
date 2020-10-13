95714a812631c925a0620d0605958a48bb6a8cf2
김지현 <ledi000@naver.com>
2020-10-13 23:39:12 +0900

    Sugar delivery(minimization problem)

---
+++ Greedy/2839/2839.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(0);
+    int n, answer = -1;
+    cin >> n;
+    if (n < 5) {
+        answer = n == 3 ? 1 : -1;
+    }
+    else if (n % 5 == 0) {
+        answer = n / 5;
+    }
+    else {
+        int savedN = n;
+        int quotient = 0;
+        while (savedN > 0) {
+            if ((n- quotient * 5) % 3 == 0) {
+                answer = quotient + (n- quotient * 5) / 3;
+            }
+            savedN -= 5;
+            quotient++; 
+        }
+    }
+    cout << answer << "\n";
+    return 0;
+}
