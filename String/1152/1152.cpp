9164d06059d179b2115af2a8740fef625343f9b5
김지현 <ledi000@naver.com>
2020-10-21 23:11:49 +0900

    Count the number of words in a string

--- String/1152/1152.cpp
+++ String/1152/1152.cpp
@@ -1,20 +1,25 @@
 #include <iostream>
 #include <string>
 using namespace std;
+int countWords(string strWithSpace) {
+    int wordCount;
+    for (int letter = 1; letter < strWithSpace.length() - 1; letter++) {
+        char space = ' ';
+        if (strWithSpace[letter] == space) {
+            wordCount++;
+        }
+    }
+    //입력으로 " "가 들어올수도 있음.
+    wordCount = strWithSpace == " " ? wordCount : wordCount + 1;
+    return wordCount;
+}
 
 int main() {
     cin.tie(nullptr);
     ios_base::sync_with_stdio(0);
     string strWithSpace;
     getline(cin, strWithSpace);
-    int cntWord = 0;
-    for (int letter = 1; letter < strWithSpace.length() - 1; letter++) {
-        if (strWithSpace[letter] == ' ') {
-            cntWord++;
-        }
-    }
-    //입력으로 " "가 들어올수도 있음.
-    cntWord = strWithSpace == " " ? cntWord : cntWord + 1;
-    cout << cntWord << "\n";
+    int wordCount = countWords(strWithSpace);
+    cout << wordCount << "\n";
     return 0;
 }
