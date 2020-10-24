4ce0868b6e0089aa556cfdba7182eddecd12260c
김지현 <ledi000@naver.com>
2020-10-24 23:37:21 +0900

    Find the most-used alphabet in a string

--- String/1157/1157.cpp
+++ String/1157/1157.cpp
@@ -2,15 +2,7 @@
 #include <cstring>
 using namespace std;
 int alphabets[26] = {0, };
-int compare(int num1, int num2) {
-    return num1 > num2;
-}
-
-int main() {
-    cin.tie(nullptr);
-    ios_base::sync_with_stdio(0);
-    string word;
-    cin >> word;
+void countEachAlphabet(string word) {
     for (int idx = 0; idx < word.length(); idx++) {
         int charToInt;
         if (65 <= word[idx] && word[idx] <= 90) {
@@ -21,19 +13,37 @@ int main() {
         }
         alphabets[charToInt]++;
     }
+}
+int findMostUsedAlphabet() {
     int maxIndex = 0;
     for (int idx = 1; idx < 26; idx++) {
         if (alphabets[maxIndex] < alphabets[idx]) {
             maxIndex = idx;
         }
     }
+    return maxIndex;
+}
+bool hasMultipleAnswer(int maxIndex) {
     int answerCnt = 0;
     for (int idx = 0; idx < 26; idx++) {
         if (alphabets[idx] == alphabets[maxIndex]) {
             answerCnt++;
+            if (answerCnt > 1) {
+                return true;
+            }
         }
     }
-    char answer = answerCnt > 1 ? '?' : maxIndex + 65;
+    return false;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(0);
+    string word;
+    cin >> word;
+    countEachAlphabet(word);
+    int maxIndex = findMostUsedAlphabet();
+    char answer = hasMultipleAnswer(maxIndex) ? '?' : maxIndex + 65;
     cout << answer << "\n";
     return 0;
 }
