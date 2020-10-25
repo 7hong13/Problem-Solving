334ea74b3550014b17bedc4b4a740f596b5d9d6d
김지현 <ledi000@naver.com>
2020-10-25 23:47:07 +0900

    Count the number of group word

--- String/1316/1316.cpp
+++ String/1316/1316.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 #include <cstring>
 using namespace std;
+bool isGroupWord(string word) {
+    int alpahbets[26] = {0, };
+    alpahbets[word[0] - 97]++;
+    for (int idx = 1; idx < word.length(); idx++) {
+        char current = word[idx];
+        char prev = word[idx - 1];
+        alpahbets[current - 97]++;
+        if (current != prev && alpahbets[current - 97] > 1) {
+            return false;
+        }
+    }
+    return true;
+}
 
 int main() {
     cin.tie(nullptr);
@@ -11,19 +24,7 @@ int main() {
     while (n--) {
         string word;
         cin >> word;
-        bool isGroupWord = true;
-        int alpahbets[26] = {0, };
-        alpahbets[word[0] - 97]++;
-        for (int idx = 1; idx < word.length(); idx++) {
-            char current = word[idx];
-            char prev = word[idx - 1];
-            alpahbets[current - 97]++;
-            if (current != prev && alpahbets[current - 97] > 1) {
-                isGroupWord = false;
-                break;
-            }
-        }
-        if (isGroupWord) groupWordNum++;
+        if (isGroupWord(word)) groupWordNum++;
     }
     cout << groupWordNum << "\n";
     return 0;
