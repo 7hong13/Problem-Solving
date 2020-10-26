501fff20b5e3717454e7d02ee2a59ac84a96f9d9
김지현 <ledi000@naver.com>
2020-10-26 23:33:32 +0900

    Count the number of each alphabet in a string

--- String/10808/10808.cpp
+++ String/10808/10808.cpp
@@ -2,12 +2,7 @@
 #include <cstring>
 using namespace std;
 int alphabets[26] = {0, };
-
-int main() {
-    cin.tie(nullptr);
-    ios_base::sync_with_stdio(0);
-    string word;
-    cin >> word;
+void countAlphabets(string word) {
     for (int letterNum = 0; letterNum < word.length(); letterNum++) {
         int charToInt = word[letterNum] - 97;
         alphabets[charToInt]++;
@@ -15,6 +10,13 @@ int main() {
     for (int idx = 0; idx < 26; idx++) {
         cout << alphabets[idx] << " ";
     }
+}
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(0);
+    string word;
+    cin >> word;
+    countAlphabets(word);
     cout << "\n";
     return 0;
 }
