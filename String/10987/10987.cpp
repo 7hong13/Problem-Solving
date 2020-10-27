d16a65cd149e43b903daa5263f7d3e05cbbc8fc7
김지현 <ledi000@naver.com>
2020-10-27 23:41:00 +0900

    Count the number of vowels in a string

--- String/10987/10987.cpp
+++ String/10987/10987.cpp
@@ -2,13 +2,8 @@
 #include <cstring>
 using namespace std;
 int g_vowelCnt = 0;
-
-int main() {
-    cin.tie(nullptr);
-    ios_base::sync_with_stdio(0);
-    string word;
-    cin >> word;
-    char vowels[] = { 'a', 'i', 'u', 'e', 'o'};
+char vowels[] = { 'a', 'i', 'u', 'e', 'o'};
+void countVowels(string word) {
     for (int idx = 0; idx < word.length(); idx++) {
         for (int vowel = 0; vowel < sizeof(vowels); vowel++) {
             if (word[idx] == vowels[vowel]) {
@@ -16,6 +11,14 @@ int main() {
             }
         }
     }
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(0);
+    string word;
+    cin >> word;
+    countVowels(word);
     cout << g_vowelCnt << "\n";
     return 0;
 }
