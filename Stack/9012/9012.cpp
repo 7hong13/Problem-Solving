a2265e11b7cddcc5bd6aa248d636dc8ddc865ad1
김지현 <ledi000@naver.com>
2020-10-28 23:48:24 +0900

    Figure out Valid Parenthesis String

--- Stack/9012/9012.cpp
+++ Stack/9012/9012.cpp
@@ -2,6 +2,23 @@
 #include <stack>
 #include <cstring>
 using namespace std;
+bool isParenthesisString(string s) {
+    if (s.size() % 2 != 0) {
+        return false;
+     }
+    stack<char> st;
+    for (int idx = 0; idx < s.size(); idx++) {
+        if (s[idx] == '(') st.push('(');
+        else if (st.empty() && s[idx] == ')') {
+            return false;
+        }
+        else {
+            st.pop();
+        }
+    }
+    bool result = st.empty() ? true : false;  
+    return result;
+}
 
 int main() {
     cin.tie(nullptr);
@@ -11,25 +28,7 @@ int main() {
     while (T--) {
         string s;
         cin >> s;
-        stack<char> st;
-        if (s.size() % 2 != 0) {
-            cout << "NO\n";
-            continue;
-        }
-        bool invalid = false;
-        for (int idx = 0; idx < s.size(); idx++) {
-            if (s[idx] == '(') st.push('(');
-            else if (st.empty() && s[idx] == ')') {
-                invalid = true;
-                cout << "NO\n";
-                break;
-            }
-            else {
-                st.pop();
-            }
-        }
-        if (invalid) continue;
-        string result = st.empty() ? "YES" : "NO";
+        string result = isParenthesisString(s) ? "YES" : "NO";
         cout << result << "\n";
     }
     return 0;
