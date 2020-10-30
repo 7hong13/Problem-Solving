f9955c59ec1c047fdbc15ed3e73b49befb914f0b
김지현 <ledi000@naver.com>
2020-10-30 23:56:00 +0900

    Queue with priority

--- Queue/1966/1966.cpp
+++ Queue/1966/1966.cpp
@@ -1,36 +1,43 @@
 #include <iostream>
 #include <queue>
 using namespace std;
+int findPrintOrder(int num, int position) {
+    queue<int> q;
+    priority_queue<int, vector<int>, less<int>> pq;
+    //set priority sequence
+    for (int idx = 0; idx < num; idx++) {
+        int priority;
+        cin >> priority;
+        q.push(priority);
+        pq.push(priority); 
+        if (idx == position) q.push(-1);
+    }
+    int popCnt = 0;
+    while (!q.empty()) {
+        int currPriority = q.front();
+        if (currPriority < pq.top()) {
+            q.pop();
+            q.push(currPriority);
+        }
+        else {
+            pq.pop();
+            q.pop();
+            popCnt++;
+            if (q.front() == -1) break;
+        }
+    }  
+    return popCnt;
+}
 
 int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(0);
     int testCase;
     cin >> testCase;
     while (testCase--) {
         int num, position, priority;
         cin >> num >> position;
-        queue<int> q;
-        priority_queue<int, vector<int>, less<int>> pq;
-        for (int idx = 0; idx < num; idx++) {
-            int priority;
-            cin >> priority;
-            q.push(priority);
-            pq.push(priority); 
-            if (idx == position) q.push(-1);
-        }
-        int popCnt = 0;
-        while (!q.empty()) {
-            int currPriority = q.front();
-            if (currPriority < pq.top()) {
-                q.pop();
-                q.push(currPriority);
-            }
-            else {
-                pq.pop();
-                q.pop();
-                popCnt++;
-                if (q.front() == -1) break;
-            }
-        }
+        int popCnt = findPrintOrder(num, position);
         cout << popCnt << "\n";
     }
     return 0;
