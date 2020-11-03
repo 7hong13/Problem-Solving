1c9a9a6494c4173f06271a064e8af73cb74beb8e
김지현 <ledi000@naver.com>
2020-11-04 02:25:30 +0900

    Distance between two points

--- Math/1004/1004.cpp
+++ Math/1004/1004.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
 using namespace std;
+int x1, y1, x2, y2, n;
+int passTimeCnt = 0;
+void countMinimumPassthrough(int n) {
+    passTimeCnt = 0;
+    while (n--) {
+        int cx, cy, r;
+        cin >> cx >> cy >> r;
+        bool isDepartureInside, isArrivalInside;
+        int distWithDeparture, distWithArrival;
+        distWithDeparture = (x1 - cx) * (x1 - cx) + (y1 - cy) * (y1 - cy);
+        distWithArrival = (x2 - cx) * (x2 - cx) + (y2 - cy) * (y2 - cy);
+        isDepartureInside = distWithDeparture <= r * r ? true : false;
+        isArrivalInside =  distWithArrival <= r * r ? true : false;
+        if (isDepartureInside != isArrivalInside) {
+            passTimeCnt++;
+        }
+    }
+}
 
 int main() {
     cin.tie(nullptr);
@@ -7,22 +25,10 @@ int main() {
     int T;
     cin >> T;
     while (T--) {
-        int x1, y1, x2, y2;
         cin >> x1 >> y1 >> x2 >> y2;
         int n;
-        int passTimeCnt = 0;
         cin >> n;
-        while (n--) {
-            int cx, cy, r;
-            cin >> cx >> cy >> r;
-            bool isDepartureInside, isArrivalInside;
-            int distWithDeparture, distWithArrival;
-            distWithDeparture = (x1 - cx) * (x1 - cx) + (y1 - cy) * (y1 - cy);
-            distWithArrival = (x2 - cx) * (x2 - cx) + (y2 - cy) * (y2 - cy);
-            isDepartureInside = distWithDeparture <= r * r ? true : false;
-            isArrivalInside =  distWithArrival <= r * r ? true : false;
-            if (isDepartureInside != isArrivalInside) passTimeCnt++;
-        }
+        countMinimumPassthrough(n);
         cout << passTimeCnt << "\n";
     }
     return 0;
