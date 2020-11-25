a755f93fc6c55b064dd4e353514255d505b9a10a
김지현 <ledi000@naver.com>
2020-11-25 23:56:55 +0900

    Find sequence of particular sum

---
+++ Math/1024/1024.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+long long n;
+int l;
+int findSequence() {
+    while (l <= 100) {
+        int sumBeforeL = (l - 1) * l / 2;
+        if ((n - sumBeforeL) % l == 0) {
+            return (n - sumBeforeL) / l;
+        }
+        l++;
+    }
+    return -1;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(0);
+    cin >> n >> l;
+    int answer = findSequence();
+    if (answer < 0) {
+        cout << "-1\n";
+    }
+    else {
+        for (int start = 0; start < l; start++) {
+            cout << answer++ << " "; 
+        }
+    }
+    return 0;
+}
