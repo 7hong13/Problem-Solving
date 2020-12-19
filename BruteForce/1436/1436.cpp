939c780c93264796d5ccea26e4d07a9c92619321
김지현 <ledi000@naver.com>
2020-12-19 23:55:28 +0900

    Find a movie title given in sequence

---
+++ BruteForce/1436/1436.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int N;
+void init() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(0);
+}
+
+void printTitle() {
+    int titleN = 0;
+    for (int num = 666; ; num++) {
+        string title = to_string(num);
+        for (int idx = 0; idx < title.size() - 2; idx++) {
+            if (title[idx] == '6' && 
+                title[idx] == title[idx + 1] && title[idx + 1] == title[idx + 2]) {
+                    titleN++;
+                    break;
+                }
+        }
+        if (titleN == N) {
+            cout << title << "\n";
+            break;
+        }
+    }
+}
+int main() {
+    init();
+    cin >> N;
+    printTitle();
+    return 0;
+}
