d5ce481f5a8919724a3252895e909e47dc14ffc2
김지현 <ledi000@naver.com>
2021-01-24 23:56:36 +0900

    Find the minimum cost and its actual path

---
+++ Dijkstra/11779/11779.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+using namespace std;
+#define MAX 1000
+#define INF 999999999
+typedef pair<int, int> pii;
+int n, m, dist[MAX + 1], nearest[MAX + 1];
+vector<pii> graph[MAX + 1];
+priority_queue<pii, vector<pii>, greater<pii>> pq;
+stack<int> path;
+void init() {
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ios_base::sync_with_stdio(0);
+    fill(dist, dist + MAX + 1, INF);
+}
+
+int dijstra(int start, int end) {
+    dist[start] = 0;
+    pq.push(pii(0, start));
+    while (!pq.empty()) {
+        int minCost = pq.top().first;
+        int u = pq.top().second;
+        pq.pop();
+        for (pii v : graph[u]) {
+            if (dist[v.first] < minCost) continue;
+            if (dist[v.first] <= dist[u] + v.second) continue;
+            dist[v.first] = dist[u] + v.second;
+            pq.push(pii(dist[v.first], v.first));
+            nearest[v.first] = u;
+        }
+    }
+    return dist[end];
+}
+
+void printSizeAndPath(int end) {
+    int v = end;
+    while (v) {
+        path.push(v);
+        v = nearest[v];
+    }
+    cout << path.size() << "\n";
+    while (!path.empty()) {
+        cout << path.top() << ' ';
+        path.pop();
+    }
+    cout << "\n";
+}
+
+int main() {
+    init();
+    cin >> n >> m;
+    while (m--) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        graph[u].push_back(pii(v, w));
+    }
+    int start, end;
+    cin >> start >> end;
+    int cost = dijstra(start, end);
+    cout << cost << "\n";
+    printSizeAndPath(end);
+    return 0;
+}
