8a3b51acfbab8380e8173d7696f04f8161412651
김지현 <ledi000@naver.com>
2021-02-10 19:34:41 +0900

    Count the number of given numbers in cards list

---
+++ BinarySearch/10816/10816.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+#define MAX 500000
+using namespace std;
+int N, M;
+int cards[MAX];
+void init() {
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ios_base::sync_with_stdio(0);
+}
+void findNumOfCards() {
+    cin >> M;
+    sort(cards, cards + N);
+    while (M--) {
+        int num;
+        cin >> num;
+        if (!binary_search(cards, cards + N, num)) cout << "0 ";
+        else {
+            int cnt = 1;
+            auto currIter = lower_bound(cards, cards + N, num);
+            auto nextIter = upper_bound(cards, cards + N, num);
+            cout << nextIter - currIter << ' ';
+        }
+    }
+}
+
+int main() {
+    init();
+    cin >> N;
+    for (int idx = 0; idx < N; idx++) {
+        cin >> cards[idx];
+    }
+    findNumOfCards();
+    return 0;
+}
